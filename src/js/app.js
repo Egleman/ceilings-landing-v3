cec6743283124935af8183e064b005f676b27be3
Maksim Sheko <sakusan@mail.ru>
2023-03-20 11:40:41 +0300

    git init

---
+++ src/js/app.js
@@ -0,0 +1,70 @@
+import * as flsFunction from "./modules/function.js";
+
+flsFunction.isWebp();
+flsFunction.calcWidthScroll();
+flsFunction.mask();
+flsFunction.openHeaderCallback();
+flsFunction.openHiddenMenu();
+flsFunction.rangeSlider();
+flsFunction.customSelect();
+flsFunction.beforAfter();
+// flsFunction.tabs();
+// flsFunction.showMoreBlocks();
+flsFunction.calc();
+flsFunction.playVideo();
+flsFunction.accordion();
+flsFunction.presentation();
+flsFunction.scrollButtons();
+flsFunction.openModal();
+flsFunction.hiddenAdv();
+
+let im = new Inputmask({
+    mask: '8 (999) 999-99-99',
+    showMaskOnHover: false,
+    showMaskOnFocus: false,
+    jitMasking: true,
+    inputmode: 'tel'
+});
+document.querySelectorAll('.masked').forEach(input => {
+    im.mask(input);
+})
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    function hasTouchDevice() {
+      return !!('ontouchstart' in window || navigator.maxTouchPoints);
+    }
+    if (!hasTouchDevice()) {
+      const toUp = document.querySelectorAll('.calc__counters-wr button');
+      toUp.forEach(link => {
+        link.classList.add('hover');
+        link.onmouseenter = function(){
+            link.classList.add('hover');
+          }
+        link.onmouseleave = function() {
+            link.classList.remove('hover');
+        }
+      })
+      
+    }
+  });
+
+// var swiper = new Swiper(".about-thumb", {
+//     modules: [FreeMode],
+//     // loop: true,
+//     spaceBetween: 10,
+//     slidesPerView: 4,
+//     // freeMode: true,
+//     watchSlidesProgress: true,
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 3,
+//         },
+//         606: {
+//             slidesPerView: 4,
+//         }
+//     }
+// });
+
